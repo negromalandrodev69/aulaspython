3905fad30d63a085aed32269e9302ae68be22b39
negromalandrodev69 <marineusaolivera371@gmail.com>
2026-09-01 19:58:56 -0300

    Conteúdo de operadores aritmedicos

---
+++ aulas/OperadoresAritmedicos.py
@@ -0,0 +1,18 @@
+#OPERADORES MATEMÁTICOS
+numero1 = 10
+numero2 = 20
+
+#SOMA
+soma = numero1 + numero2
+#SUTRAÇÃO
+subtração = numero1 - numero2
+#MULTIPLICAÇÃO
+multiplicacao = numero1 * numero2
+#DIVISÃO
+divisao = numero1 / numero2
+resto = numero1 % numero2
+
+print("Soma dos números", soma)
+print("Subtração dos números", subtração)
+print("Multiplicação dos números", multiplicacao)
+print("Resto dos números", resto)
