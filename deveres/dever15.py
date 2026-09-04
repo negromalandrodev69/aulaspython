2a0f93c73a362d9c6ac8a92617964a8e5fa8bfeb
negromalandrodev69 <marineusaolivera371@gmail.com>
2026-09-04 20:50:37 -0300

    deveres

--- deveres/dever15.py
+++ deveres/dever15.py
@@ -1,2 +1,5 @@
 numero1 = int(input("Olá pode digitar um numero?"))
-
+if numero1 % 2 == 0:
+    print("O número"+ numero1 + "é PAR")
+else:
+    print("O número" + numero1 + "é ÍMPAR")
---
+++ deveres/dever20.py
@@ -0,0 +1,11 @@
+print("Qual produto queres? (1) (2) (3) (4)")
+comida = int(input())
+match comida:
+    case 1:
+        print("Cachorro-quente R$ 10,00.")
+    case 2:
+        print("Hamnbúrger R$ 15,00.")
+    case 3:
+        print("Batata Frita R$ 8,00.")
+    case 4:
+        print("Refrigerante R$ 5,00.")
