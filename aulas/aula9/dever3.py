e92e1a0854a7f08e836fbbd99393a16831841ecd
negromalandrodev69 <marineusaolivera371@gmail.com>
2026-09-09 20:32:19 -0300

    deveres

--- aulas/aula9/dever3.py
+++ aulas/aula9/dever3.py
@@ -1,5 +1,8 @@
 numero = int(input("digite numeros aleatoriamente: "))
+numero_soma = 0
 while numero != 0:
+    numero_soma += numero
     numero = int(input("digite mais numeros aleatoriamente: "))
     if numero == 0:
+        print("A soma dos numeros é ", numero_soma)
         break
