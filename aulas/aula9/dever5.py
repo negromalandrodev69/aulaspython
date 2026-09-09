ae17d786c5518266bb0354dde4575ea0c07e1c95
negromalandrodev69 <marineusaolivera371@gmail.com>
2026-09-09 20:40:04 -0300

    deveres

--- aulas/aula9/dever5.py
+++ aulas/aula9/dever5.py
@@ -1,1 +1,14 @@
 # Tabuada simples
+numerom = int(input("Fale qual nmero quer saber a tabuada: "))
+while numerom :
+    print(numerom,"x", numerom * 1)
+    print(numerom,"x", numerom * 2)
+    print(numerom,"x", numerom * 3)
+    print(numerom,"x", numerom * 4)
+    print(numerom,"x", numerom * 5)
+    print(numerom,"x", numerom * 6)
+    print(numerom,"x", numerom * 7)
+    print(numerom,"x", numerom * 8)
+    print(numerom,"x", numerom * 9)
+    print(numerom,"x", numerom * 10)
+    break
