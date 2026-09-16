334dab0ca085034e4f43a43dc7d834a48aadf30f
negromalandrodev69 <marineusaolivera371@gmail.com>
2026-09-16 19:41:58 -0300

    deveres

--- aulas/aula11/dever2.py
+++ aulas/aula11/dever2.py
@@ -2,6 +2,9 @@
 #Sua função deve calcular a média final desse aluno, e imprimir na tela todos os valores, e informar se o aluno foi reprovado ou aprovado pela média final.
 #OBS: valor da media = 7
 #usar "def"
+from unittest import case
+
+
 def media():
     nome = str(input("Coloque o nome do aluno: "))
     nota1 = float(input("Digite a nota do primeiro semestre: "))
@@ -9,5 +12,24 @@ def media():
     nota3 = float(input("Digite a nota do terceiro semestre: "))
     nota4 = float(input("Digite a nota do quarto semestre: "))
     nota_final = nota1 + nota2 + nota3 + nota4 / 2
-    return nome, nota_final
+    match nota_final >= 7:
+        case True:
+            print("A o aluno: ",
+                 nome,
+                 "Tirou essas notas : ",
+                 nota1,
+                 nota2,
+                 nota3,
+                 nota4,
+                 "Ele passou da media final")
+        case False:
+            print("A o aluno: ",
+                 nome,
+                 "Tirou essas notas : ",
+                 nota1,
+                 nota2,
+                 nota3,
+                 nota4,
+                 "Ele não passou da media final")
+    return
 print(media())
